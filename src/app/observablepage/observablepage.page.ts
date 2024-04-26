bceaeb93dd11c8ca2a9487aedb9943a09581282e
YuliaBezhenar <ulabusinka@gmail.com>
2024-04-26 11:51:56 +0300

    lab6

---
+++ src/app/observablepage/observablepage.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-observablepage',
+  templateUrl: './observablepage.page.html',
+  styleUrls: ['./observablepage.page.scss'],
+})
+export class ObservablepagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
